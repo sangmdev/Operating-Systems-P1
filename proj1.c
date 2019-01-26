de00aad4f16ed8d0ed9893211974c0364f5c51fb
Sang Mai <sangmai913@gmail.com>
2019-01-26 14:56:03 -0600

    added ability for options i and o to recognize if there is an argument or not

--- proj1.c
+++ proj1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 int main(int argc, char **argv){
   int c;
-  printf("Hello World\n");
   while((c = getopt(argc, argv, "hi::o::")) != -1)
      switch(c)
      {
@@ -11,16 +11,26 @@ int main(int argc, char **argv){
         printf("Legal Command line options: -h, -i, -o\n");
 	printf("-i is the input file and should have an input file name afterwards\n");
 	printf("-o is the output file and should have an output file name afterwards\n");
-	printf("If either input file name or output file name is not specified, the defaults input.dat and output.dat will be used respectively./n");
+	printf("If either input file name or output file name is not specified, the defaults input.dat and output.dat will be used respectively.\n");
 	return 0;
      case 'i':
-     	printf("case i");
+	if(optarg == NULL){
+	   printf("no input file");
+	}
+	else{
+	   printf("%s", optarg);
+	   }
      	break;
      case 'o':
-     	printf("case o");
+     	if(optarg == NULL){
+	   printf("no output file");
+	}
+	else{
+	    printf("%s", optarg);
+	}
 	break;
      case '?':
-        printf("%s", argv[0]);
+        printf("Invalid option");
 	break;
      default:
         printf("getopt didn't work");
