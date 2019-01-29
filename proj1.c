1ab6ed976f17ed90d15e13779a9bd7ec533b591a
Sang Mai <sangmai913@gmail.com>
2019-01-29 10:37:57 -0600

    Child processes open up the inputfile and reads the next line and loop reads the next integers, updated README

--- proj1.c
+++ proj1.c
@@ -33,27 +33,43 @@ int main(int argc, char **argv){
 	break;
      case '?':
 	printf("invalid option used");
+	return 0;
 	break;
      default:
         printf("getopt didn't work");
+	return 0;
+	break;
      }
      
      //BEGIN FILE PROCESSING//
      FILE *fp;
-     int count = 0, i;;
+     int count = 0, i;
      fp = fopen(inputFileName, "r");
      fscanf(fp, "%d", &count);
      
-     //FORKS//
-     pid_t child_pid, wpid;
-     int status =0;
+     //Child Processes in a loop//
+       pid_t child_pid, wpid;
+       int status =0;
+       int stack[8];
      for (i = 0; i < count; i++){
+       if(i > 0){
+          sleep(10+i);
+       }
        if ((child_pid = fork()) == 0) {
-          printf("Hello from child\n");  
-          exit(0);
+	  int numOfNums;
+	  fscanf(fp, "%d", &numOfNums); 
+          for(i = 0; i < numOfNums;i++){
+	     fscanf(fp, "%d", &stack[i]);
+	     printf("%d ", stack[i]);
+	  }
+	  printf("%d\n", getpid());
+	  exit(0);
        }
      }
+     //PARENT PROCESS//
      while((wpid = wait(&status)) > 0);
-     printf("Hello from parent");
+     printf("Hello from parent\n");
+
+     fclose(fp);
 return 0;
 }
